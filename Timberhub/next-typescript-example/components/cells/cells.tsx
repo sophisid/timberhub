4d84c126ebf687c9b336f4cedfc8ca3d82250f04
sophisid <sophiasideris@gmail.com>
2022-12-07 18:29:41 +0200

    added classes to cell components

--- Timberhub/next-typescript-example/components/cells/cells.tsx
+++ Timberhub/next-typescript-example/components/cells/cells.tsx
@@ -3,6 +3,7 @@ import { Company } from '../../model/company';
 import { CProgressBar } from '@coreui/react';
 import { CProgress } from '@coreui/react';
 import styles from "./cells.module.scss";
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
 
 export interface ProjectNameProps {
    projName:string,
@@ -17,35 +18,53 @@ export interface ProjectNameProps {
    }
 
 const ProjectName = (props: ProjectNameProps) => (
-    	<div>
-            {props.img}
-    		{props.companyName}
-            {props.projName}
+    	<div className={styles.projcell}>
+            <img src={props.img} />
+            <div className={styles.projcell_text}>
+                <div className={styles.projcell_text_projName}>{props.projName}</div>
+                <div className={styles.projcell_text_companyName}>{props.companyName}</div>
+            </div>
     	</div>
     );
 
     const CreatedBy = (props: PersonCellProps) => (
-    	<div>
-    		{props.img}
-    		{props.personName}
-            {props.coccupation}
+    	<div className={styles.projcell}>
+            <img src={props.img} />
+            <div className={styles.projcell_text}>
+                <div className={styles.projcell_text_projNamevc}>{props.personName}</div>
+                <div className={styles.projcell_text_companyName}>{props.coccupation}</div>
+            </div>
     	</div>
     );
     const ProgressCell = (progress:number) =>{
-        <CProgress className="mb-3">
-            <CProgressBar color="success" value={progress}/>
-        </CProgress>
+        <div>
+            <CProgress className="mb-3">
+                <CProgressBar color="success" value={progress}/>
+            </CProgress>
+        <div>
+            {progress} %
+        </div>
+        </div>
     }
 
     const DeadlineCell = (left:number, type:string) =>{
-        <div className={styles.chip}>
-            <div>
-                {/* clock icon */}
+       {type === "days" ? ( <div className={styles.chipOrange}>
+            <div className={styles.chipOrange_clock}>
+                <AccessTimeIcon sx={{color:'#E56E19'}}/>
             </div>
-            <div>
+            <div className={styles.chipOrange_text}>
                 {left} {type} left
             </div>
         </div>
+        ):(
+        <div className={styles.chipGrey}>
+            <div className={styles.chipGrey_clock}>
+                <AccessTimeIcon sx={{color:'#3F434A'}}/>
+            </div>
+            <div className={styles.chipGrey_text}>
+                {left} {type} left
+            </div>
+        </div>)}
     }
 
     export {ProjectName ,CreatedBy, ProgressCell, DeadlineCell};
